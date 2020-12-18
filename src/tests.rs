4378bd4c8a9aa2267426f90cfce3e51ccd6009eb
Bachmann, Rafael <rafael.bachmann.93@gmail.com>
2020-12-18 16:26:49 +0100

    Add more tests

--- src/tests.rs
+++ src/tests.rs
@@ -25,7 +25,17 @@ impl CommandHandler for Thing {
     }
 
     fn handle_read_word_data(&self, reg: u8) -> Option<u16> {
-        unimplemented!()
+        match reg {
+            7 => {
+                let data = self.byte_a as u16 | (self.byte_b as u16) << 8;
+                Some(data)
+            },
+            8 => {
+                let data = self.byte_b as u16 | (self.byte_c as u16) << 8;
+                Some(data)
+            },
+            _ => None,
+        }
     }
 
     fn handle_read_block_data(&self, reg: u8, index: u8) -> Option<u8> {
@@ -42,11 +52,41 @@ impl CommandHandler for Thing {
     }
 
     fn handle_write_byte_data(&mut self, reg: u8, data: u8) -> Result<(), ()> {
-        unimplemented!()
+        match reg {
+            4 => {
+                self.byte_a = data;
+                Ok(())
+            },
+            5 => {
+                self.byte_b = data;
+                Ok(())
+            },
+            6 => {
+                self.byte_c = data;
+                Ok(())
+            }
+            _ => Err(())
+        }
     }
 
     fn handle_write_word_data(&mut self, reg: u8, data: u16) -> Result<(), ()> {
-        unimplemented!()
+        match reg {
+            9 => {
+                let data1 = data as u8;
+                let data2 = (data >> 8) as u8;
+                self.byte_a = data1;
+                self.byte_b = data2;
+                Ok(())
+            },
+            10 => {
+                let data1 = data as u8;
+                let data2 = (data >> 8) as u8;
+                self.byte_b = data1;
+                self.byte_c = data2;
+                Ok(())
+            },
+            _ => Err(()),
+        }
     }
 
     fn handle_write_block_data(&mut self, reg: u8, count: u8, block: [u8; 32]) -> Result<(), ()> {
@@ -140,5 +180,96 @@ fn test_read_byte_data() {
     assert_eq!(0x76, data);
     assert_eq!(0x76, thing.byte_a);
     assert_eq!(0x0a, thing.byte_b);
+    assert_eq!(0x0b, thing.byte_c);
+}
+
+#[test]
+fn test_read_word_data() {
+    let mut thing = Thing {
+        byte_a: 0x76,
+        byte_b: 0x0a,
+        byte_c: 0x0b,
+    };
+    let mut bus_state = SMBusState::default();
+
+    let mut event = I2CEvent::Initiated {
+        direction: Direction::MasterToSlave,
+    };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::ReceivedByte { byte: 8 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Initiated { direction: Direction::SlaveToMaster };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    let mut data1 = 0;
+    event = I2CEvent::RequestedByte { byte: &mut data1 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    let mut data2 = 0;
+    event = I2CEvent::RequestedByte { byte: &mut data2 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Stopped;
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    assert_eq!(0x0a, data1);
+    assert_eq!(0x0b, data2);
+}
+
+#[test]
+fn test_write_byte_data() {
+    let mut thing = Thing {
+        byte_a: 0x76,
+        byte_b: 0x0a,
+        byte_c: 0x0b,
+    };
+    let mut bus_state = SMBusState::default();
+
+    let mut event = I2CEvent::Initiated {
+        direction: Direction::MasterToSlave,
+    };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::ReceivedByte { byte: 5 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::ReceivedByte { byte: 0x76 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Stopped;
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    assert_eq!(0x76, thing.byte_b);
+    assert_eq!(0x76, thing.byte_a);
+    assert_eq!(0x0b, thing.byte_c);
+}
+
+#[test]
+fn test_write_word_data() {
+    let mut thing = Thing {
+        byte_a: 0x76,
+        byte_b: 0x0a,
+        byte_c: 0x0b,
+    };
+    let mut bus_state = SMBusState::default();
+
+    let mut event = I2CEvent::Initiated {
+        direction: Direction::MasterToSlave,
+    };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::ReceivedByte { byte: 5 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::ReceivedByte { byte: 0x76 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Stopped;
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    assert_eq!(0x76, thing.byte_b);
+    assert_eq!(0x76, thing.byte_a);
     assert_eq!(0x0b, thing.byte_c);
 }
