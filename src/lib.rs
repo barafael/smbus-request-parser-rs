1b85b3b2d48e4946820272a824df0da58c8b5e46
Bachmann, Rafael <rafael.bachmann.93@gmail.com>
2020-12-18 12:51:59 +0100

    Add tests for command handler trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,137 @@
 
 #[cfg(test)]
 extern crate std;
+
 #[cfg(test)]
 mod tests;
 
 trait CommandHandler {
+    fn read_byte_supported(&self) -> bool;
     fn handle_read_byte(&self) -> Option<u8>;
     fn handle_read_byte_data(&self, reg: u8) -> Option<u8>;
     fn handle_read_word_data(&self, reg: u8) -> Option<u16>;
     fn handle_read_block_data(&self, reg: u8, index: u8) -> Option<u8>;
 
+    fn write_byte_supported(&self) -> bool;
     fn handle_write_byte(&mut self, data: u8) -> Result<(), ()>;
     fn handle_write_byte_data(&mut self, reg: u8, data: u8) -> Result<(), ()>;
     fn handle_write_word_data(&mut self, reg: u8, data: u16) -> Result<(), ()>;
     fn handle_write_block_data(&mut self, reg: u8, count: u8, block: [u8; 32]) -> Result<(), ()>;
+
+    fn handle_i2c_event(
+        &mut self,
+        event: &mut I2CEvent,
+        bus_state: &mut SMBusState,
+    ) -> Result<(), SMBusProtocolError> {
+        match event {
+            I2CEvent::Initiated { direction } => bus_state.direction = Some(*direction),
+            I2CEvent::ReceivedByte { byte } => {
+                bus_state.received_data[bus_state.index as usize] = *byte;
+                bus_state.index += 1;
+                if bus_state.index == 35 {
+                    return Err(SMBusProtocolError::UnsupportedBlockLength);
+                }
+            }
+            I2CEvent::RequestedByte { byte } => {
+                if bus_state.direction != Some(Direction::SlaveToMaster) {
+                    return Err(SMBusProtocolError::WrongDirection(bus_state.direction));
+                }
+                match bus_state.index {
+                    0 => {
+                        if let Some(Direction::SlaveToMaster) = bus_state.direction {
+                            if let Some(data) = self.handle_read_byte() {
+                                **byte = data;
+                            } else {
+                                return Err(SMBusProtocolError::ReadByteUnsupported);
+                            }
+                        } else {
+                            return Err(SMBusProtocolError::WrongDirection(bus_state.direction));
+                        }
+                    }
+                    1 => {
+                        let first_byte = bus_state.received_data[0];
+                        if let Some(data) = self.handle_read_byte_data(first_byte) {
+                            **byte = data;
+                        } else if let Some(data) = self.handle_read_word_data(first_byte) {
+                            bus_state.current_transfer = Some(StatefulTransfer::ReadWord(data));
+                            **byte = data as u8;
+                        } else if let Some(data) = self.handle_read_block_data(first_byte, 0) {
+                            bus_state.current_transfer = Some(StatefulTransfer::ReadBlock(data));
+                            **byte = data;
+                        } else {
+                            return Err(SMBusProtocolError::InvalidReadRegister(first_byte));
+                        }
+                    }
+                    2 => {
+                        let first_byte = bus_state.received_data[0];
+                        match bus_state.current_transfer {
+                            Some(StatefulTransfer::ReadWord(data)) => {
+                                **byte = (data >> 8) as u8;
+                                bus_state.current_transfer = None;
+                            }
+                            Some(StatefulTransfer::ReadBlock(count)) => {
+                                self.handle_read_block_data(first_byte, 1);
+                            }
+                            _ => return Err(SMBusProtocolError::InvalidReadBound(2)),
+                        }
+                    }
+                    n => {
+                        if let Some(StatefulTransfer::ReadBlock(count)) = bus_state.current_transfer
+                        {
+                            if let Some(data) =
+                                self.handle_read_block_data(bus_state.received_data[0], n - 1)
+                            {
+                                **byte = data;
+                            }
+                        }
+                    }
+                }
+                bus_state.index += 1;
+            }
+            I2CEvent::Stopped => {
+                if bus_state.direction == Some(Direction::MasterToSlave) {
+                    match bus_state.index {
+                        0 => return Err(SMBusProtocolError::QuickCommandUnsupported),
+                        1 => {
+                            if let Err(()) = self.handle_write_byte(bus_state.received_data[0]) {
+                                return Err(SMBusProtocolError::WriteByteUnsupported);
+                            }
+                        }
+                        2 => {
+                            if let Err(()) = self.handle_write_byte_data(
+                                bus_state.received_data[0],
+                                bus_state.received_data[1],
+                            ) {
+                                return Err(SMBusProtocolError::InvalidWriteRegister(
+                                    bus_state.received_data[0],
+                                ));
+                            }
+                        }
+                        3 => {
+                            let data: u16 = bus_state.received_data[1] as u16
+                                | (bus_state.received_data[2] as u16) << 8;
+                            if let Err(()) =
+                                self.handle_write_word_data(bus_state.received_data[0], data)
+                            {
+                                return Err(SMBusProtocolError::InvalidWriteRegister(
+                                    bus_state.received_data[0],
+                                ));
+                            };
+                        }
+                        4..=32 => {
+                            // TODO increase buffer size to accommodate actual 32byte block transfers (right now register and block take a byte each)
+                            let count = bus_state.received_data[1];
+                        }
+                        _ => unreachable!(),
+                    };
+                }
+                bus_state.received_data.iter_mut().for_each(|x| *x = 0);
+                bus_state.index = 0;
+                bus_state.direction = None;
+            }
+        }
+        return Ok(());
+    }
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -33,7 +151,7 @@ pub enum I2CEvent<'a> {
 #[derive(Debug)]
 enum StatefulTransfer {
     ReadWord(u16),
-    ReadBlock(u8, [u8; 32]),
+    ReadBlock(u8),
 
     WriteWord(u16),
     WriteBlock(u8, [u8; 32]),
@@ -60,89 +178,4 @@ pub enum SMBusProtocolError {
     InvalidReadBlockSize(u8),
     InvalidReadRegister(u8),
     InvalidWriteRegister(u8),
-}
-
-impl dyn CommandHandler {
-    pub fn handle_i2c_event(
-        &mut self,
-        event: &mut I2CEvent,
-        bus_state: &mut SMBusState,
-    ) -> Result<(), SMBusProtocolError> {
-        match event {
-            I2CEvent::Initiated { direction } => bus_state.direction = Some(*direction),
-            I2CEvent::ReceivedByte { byte } => {
-                bus_state.received_data[bus_state.index as usize] = *byte;
-                bus_state.index += 1;
-                if bus_state.index == 35 {
-                    return Err(SMBusProtocolError::UnsupportedBlockLength);
-                }
-            }
-            I2CEvent::RequestedByte { byte } => {
-                match bus_state.index {
-                    0 => {
-                        if let Some(Direction::SlaveToMaster) = bus_state.direction {
-                            if let Some(data) = self.handle_read_byte() {
-                                **byte = data;
-                            } else {
-                                return Err(SMBusProtocolError::ReadByteUnsupported);
-                            }
-                        } else {
-                            return Err(SMBusProtocolError::WrongDirection(bus_state.direction));
-                        }
-                    },
-                    n => {
-                        let first_byte = bus_state.received_data[0];
-                        if n == 1 {
-                            if let Some(data) = self.handle_read_byte_data(first_byte) {
-                                **byte = data;
-                            }
-                            if let Some(data) = self.handle_read_word_data(first_byte) {
-                                bus_state.current_transfer = Some(StatefulTransfer::ReadWord(data));
-                                **byte = data as u8;
-                            } else {
-                                return Err(SMBusProtocolError::InvalidReadRegister(first_byte));
-                            }
-                        } else if n == 2 {
-                            match bus_state.current_transfer {
-                                Some(StatefulTransfer::ReadWord(data)) => {
-                                    **byte = (data >> 8) as u8;
-                                    bus_state.current_transfer = None;
-                                },
-                                _ => return Err(SMBusProtocolError::InvalidReadBound(2)),
-                            }
-                        } else {
-                            return Err(SMBusProtocolError::InvalidReadBound(2));
-                        }
-                    }
-                }
-                bus_state.index += 1;
-            }
-            I2CEvent::Stopped => {
-                match bus_state.index {
-                    0 => return Err(SMBusProtocolError::QuickCommandUnsupported),
-                    1 => if let Err(()) = self.handle_write_byte(bus_state.received_data[0]) {
-                        return Err(SMBusProtocolError::WriteByteUnsupported);
-                    }
-                    2 => if let Err(()) = self.handle_write_byte_data(bus_state.received_data[0], bus_state.received_data[1]) {
-                        return Err(SMBusProtocolError::InvalidWriteRegister(bus_state.received_data[0]));
-                    }
-                    3 => {
-                        let data: u16 = bus_state.received_data[1] as u16 | (bus_state.received_data[2] as u16) << 8;
-                        if let Err(()) = self.handle_write_word_data(bus_state.received_data[0], data) {
-                            return Err(SMBusProtocolError::InvalidWriteRegister(bus_state.received_data[0]));
-                        };
-                    }
-                    4..=32 => {
-                        // TODO increase buffer size to accommodate actual 32byte block transfers (right now register and block take a byte each)
-                        let count = bus_state.received_data[1];
-                    }
-                    _ => unreachable!(),
-                };
-                bus_state.received_data.iter_mut().for_each(|x| *x = 0);
-                bus_state.index = 0;
-                bus_state.direction = None;
-            }
-        }
-        return Ok(());
-    }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,1 +1,144 @@
 use crate::*;
+
+struct Thing {
+    byte_a: u8,
+    byte_b: u8,
+    byte_c: u8,
+}
+
+impl CommandHandler for Thing {
+    fn read_byte_supported(&self) -> bool {
+        true
+    }
+
+    fn handle_read_byte(&self) -> Option<u8> {
+        Some(self.byte_a)
+    }
+
+    fn handle_read_byte_data(&self, reg: u8) -> Option<u8> {
+        match reg {
+            1 => Some(self.byte_a),
+            2 => Some(self.byte_b),
+            3 => Some(self.byte_c),
+            _ => None,
+        }
+    }
+
+    fn handle_read_word_data(&self, reg: u8) -> Option<u16> {
+        unimplemented!()
+    }
+
+    fn handle_read_block_data(&self, reg: u8, index: u8) -> Option<u8> {
+        unimplemented!()
+    }
+
+    fn write_byte_supported(&self) -> bool {
+        true
+    }
+
+    fn handle_write_byte(&mut self, data: u8) -> Result<(), ()> {
+        self.byte_a = data;
+        Ok(())
+    }
+
+    fn handle_write_byte_data(&mut self, reg: u8, data: u8) -> Result<(), ()> {
+        unimplemented!()
+    }
+
+    fn handle_write_word_data(&mut self, reg: u8, data: u16) -> Result<(), ()> {
+        unimplemented!()
+    }
+
+    fn handle_write_block_data(&mut self, reg: u8, count: u8, block: [u8; 32]) -> Result<(), ()> {
+        unimplemented!()
+    }
+}
+
+#[test]
+fn test_read_byte() {
+    let mut thing = Thing {
+        byte_a: 0x42,
+        byte_b: 0x10,
+        byte_c: 0x20,
+    };
+    let mut bus_state = SMBusState::default();
+
+    let mut event = I2CEvent::Initiated {
+        direction: Direction::SlaveToMaster,
+    };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    let mut data = 0;
+    event = I2CEvent::RequestedByte { byte: &mut data };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Stopped;
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    assert_eq!(0x42, data);
+    assert_eq!(0x42, thing.byte_a);
+    assert_eq!(0x10, thing.byte_b);
+    assert_eq!(0x20, thing.byte_c);
+}
+
+#[test]
+fn test_write_byte() {
+    let mut thing = Thing {
+        byte_a: 0x76,
+        byte_b: 0x0a,
+        byte_c: 0x0b,
+    };
+    let mut bus_state = SMBusState::default();
+
+    let mut event = I2CEvent::Initiated {
+        direction: Direction::MasterToSlave,
+    };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    assert_eq!(0x76, thing.byte_a);
+
+    event = I2CEvent::ReceivedByte { byte: 0x34 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Stopped;
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    assert_eq!(0x34, thing.byte_a);
+    assert_eq!(0x0a, thing.byte_b);
+    assert_eq!(0x0b, thing.byte_c);
+}
+
+#[test]
+fn test_read_byte_data() {
+    let mut thing = Thing {
+        byte_a: 0x76,
+        byte_b: 0x0a,
+        byte_c: 0x0b,
+    };
+    let mut bus_state = SMBusState::default();
+
+    let mut event = I2CEvent::Initiated {
+        direction: Direction::MasterToSlave,
+    };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::ReceivedByte { byte: 1 };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Initiated {
+        direction: Direction::SlaveToMaster,
+    };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    let mut data = 0;
+    event = I2CEvent::RequestedByte { byte: &mut data };
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    event = I2CEvent::Stopped;
+    thing.handle_i2c_event(&mut event, &mut bus_state).unwrap();
+
+    assert_eq!(0x76, data);
+    assert_eq!(0x76, thing.byte_a);
+    assert_eq!(0x0a, thing.byte_b);
+    assert_eq!(0x0b, thing.byte_c);
+}
